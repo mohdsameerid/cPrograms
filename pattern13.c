c001c33021974ea1e4adaee3f1a90fbb229591d5
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-12 23:22:24 +0530

    Some changes

--- pattern13.c
+++ pattern13.c
@@ -3,14 +3,11 @@ void main(){
     char a;
     for(i=1; i<=4; i++){
         a=65;
-        for(j=1; j<=7; j++)
-        {
-            if(j<=5-i || j>=3+i)
-            {
+        for(j=1; j<=7; j++){
+            if(j<=5-i || j>=3+i){
                 printf(" %c",a);
             }
-            else
-            {
+            else{
                 printf("  ");
             }
             a++;
